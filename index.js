ba807792032487fca3d09426113148d94407cedd
Adam Scott <adam.scott@cfpb.gov>
2014-07-08 16:25:42 -0400

    scrape both text and xml

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var mkdirp = require('mkdirp');
 
 var args = process.argv.slice(2);
 var year;
+var regs = {};
 
 if (args.length > 0) {
   year = args[0];
@@ -16,43 +17,56 @@ if (args.length > 0) {
 
 var url = 'http://www.gpo.gov/fdsys/browse/collectionCfr.action?selectedYearFrom=' + year + '&go=Go';
 
-function makeDir(){
-  mkdirp('./text', function (err) {
+function makeDir(type){
+  mkdirp('./' + type, function (err) {
     if (err) console.error(err);
   });
 }
 
-function writeFiles(err, fileName, fileContent) {
-  fs.writeFile('text/' + fileName, fileContent, function(err) {
+function writeFiles(err, fileName, fileContent, type) {
+  var filePath = './' + type + '/' + fileName;
+  fs.writeFile(filePath, fileContent, function(err) {
     if(err) console.error(err);
     else console.log(fileName + ' was saved!');
   });
 }
 
-function saveFiles(links) {
-  links.forEach(function(regURL) {
-    var simpleFileName = regURL.split('/').slice(-1)[0].split('.').slice(0)[0] + '.txt';
-    request(regURL, function (error, response, html) {
-      writeFiles(error, simpleFileName, html);
-    });
+function saveFiles(regs, type, extension) {
+  makeDir(type);
+  Object.keys(regs).forEach(function(regName) {
+    var fileName = regName + extension
+    var fileURL = regs[regName][type];
+    if ( fileURL ) {
+      request(fileURL, function (error, response, body) {
+        writeFiles(error, fileName, body, type);
+      });
+    }
   });
 }
 
-function getURLs ($, type) {
+function getURLs ($) {
   var linkList = [];
 
-  $('.cfr-download-links a:contains("Text")').each(function(index) {
-    linkList.push($(this).attr('href'));
+  $('.cfr-download-links a').each(function(index) {
+    var href = $(this).attr('href');
+    var fileType = $(this).text().trim().toLowerCase();
+    var regName = href.split('/').slice(-1)[0].split('.').slice(0)[0];
+    if (fileType === 'text' || fileType === 'pdf' || fileType === 'xml') {
+      if (!regs[regName]) {
+        regs[regName] = {};
+      }
+      regs[regName][fileType] = href;
+    }
   });
 
-  makeDir();
-  saveFiles(linkList);
+  saveFiles(regs, 'text', '.txt');
+  saveFiles(regs, 'xml', '.xml');
 }
 
 request(url, function (error, response, html) {
   if (!error && response.statusCode == 200) {
     var $ = cheerio.load(html);
 
-    getURLs($, 'Text');
+    getURLs($);
   }
 });
